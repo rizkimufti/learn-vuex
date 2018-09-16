d8d2f5dceaf136e9361e88a8dd06298d490750e9
rizkimufti <rizkimufti@gmail.com>
2018-09-16 21:28:05 +0700

    update productlist

--- src/components/ ProductList.vue
+++ src/components/ ProductList.vue
@@ -1,24 +1,25 @@
+
 <template>
     <div>
       <h1>Product List</h1>
       <ul>
-
-    <li v-for="product in products"> {{product.title}} - {{product.price}}</li>      
+        <li v-for="product in products">{{product.title}} - {{product.price}}</li>
       </ul>
     </div>
 </template>
 
 <script>
   import shop from '@/api/shop'
+  import store from '@/store/index'
   export default {
-    data () {
-      return {
-        products: []
+    computed: {
+      products () {
+        return store.state.products
       }
     },
     created () {
       shop.getProducts(products => {
-         this.products = products
+        store.commit('setProducts', products)
       })
     }
   }
