c5c679a7f3971fdff5c99c15f05d2bae225eb980
rizkimufti <rizkimufti@gmail.com>
2018-09-17 21:45:34 +0700

    inject store to components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import store from '@/store/index'
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  rstore,
   render: h => h(App)
 })
